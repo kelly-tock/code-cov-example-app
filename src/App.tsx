2ee7046e301b3c19b793fa8afefee1ce77ca96e2
kellyrmilligan <kelly.milligan@gmail.com>
2021-05-19 13:41:37 -0500

    test out stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "./Button";
 
 function App() {
   return (
@@ -19,6 +20,25 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <h2>Add components here to see what happens to coverage!</h2>
+        <ol>
+          <li>Create a new component and add it to this page</li>
+          <li>Commit that component and change to app in a PR.</li>
+          <li>See the affect on code coverage</li>
+          <li>Write a test for the component you added and update the PR</li>
+          <li>See the affect on code coverage</li>
+        </ol>
+        <p>
+          <Button>Default</Button>
+        </p>
+        <p>
+          <Button variant="primary">Primary</Button>
+        </p>
+        <p>
+          <Button variant="secondary">Secondary</Button>
+        </p>
+      </main>
     </div>
   );
 }
