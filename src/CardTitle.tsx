e0ec0d9d8c133b50420e1c8bb7008ba5c07c5d83
kellyrmilligan <kelly.milligan@gmail.com>
2021-05-20 09:55:18 -0500

    card title

---
+++ src/CardTitle.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from "react";
+import clsx from "clsx";
+import "./CardTitle.css";
+
+interface CardTitleProps {
+  title: string;
+  subtitle?: string;
+}
+
+export const Card: FunctionComponent<CardTitleProps> = ({
+  title,
+  subtitle,
+}) => {
+  return (
+    <div className="CardTitle">
+      <h2>{title}</h2>
+      {subtitle ? <p>{subtitle}</p> : null}
+    </div>
+  );
+};
